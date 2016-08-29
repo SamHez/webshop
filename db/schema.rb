21dbc96406aafadc4011b31366510565e29815db
alex shyaka <shyakaster@gmail.com>
2016-08-29 21:39:13 +0300

    add blog migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829093519) do
+ActiveRecord::Schema.define(version: 20160829164647) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.text     "content",    limit: 65535
+    t.string   "author",     limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "title",      limit: 255
+  end
 
   create_table "image_tables", force: :cascade do |t|
     t.string "image_name", limit: 255
