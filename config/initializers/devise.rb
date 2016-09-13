cf363ebb7b0b51ed555f77cb0ade821386b610ce
alex shyaka <shyakaster@gmail.com>
2016-09-13 13:43:48 +0300

    cahnged default sender email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'alex@therailsshop.club'
+  config.mailer_sender = 'donotreply@therailsshop.club'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
